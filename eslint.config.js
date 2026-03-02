984c760f7a62d3b5cd507ab1cd9b1024e43615a8
root <root@DESKTOP-5G3GC89.localdomain>
2026-03-02 16:51:01 +0200

    fix: resolve security warnings and update eslint ignores

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,10 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 import pluginSecurity from "eslint-plugin-security";
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
+    ignores: ["dist/**", "coverage/**"],
+    plugins: { js }, extends: ["js/recommended"], 
+    languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginSecurity.configs.recommended,
 
